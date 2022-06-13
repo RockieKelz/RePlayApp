819c325d569ce41dcaa16f301df39d22d577d55c
RockieKelz <racquy@msn.com>
2022-06-12 23:14:08 -0700

    Added section to sidebar that shows the users playlists

---
+++ RePlayApp/src/components/Playlists.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { casesReducer } from "../utils/constants";
+import { useStateProvider } from "../utils/stateprovider";
+
+export default function Playlists() {
+  //Get access token and users playlists
+  const [{ token, playlists }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const getPlaylistData = async () => {
+      const response = await axios.get(
+        "https://api.spotify.com/v1/me/playlists",
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const { items } = response.data;
+      const playlists = items.map(({ name, id }) => {
+        return { name, id };
+      });
+      dispatch({ type: casesReducer.SET_PLAYLISTS, playlists });
+    };
+    getPlaylistData();
+  }, [token, dispatch]);
+
+  const changeCurrentPlaylist = (selectedPlaylistId) => {
+    dispatch({ type: casesReducer.SET_PLAYLIST_ID, selectedPlaylistId });
+  };
+  return (
+    <Container>
+      <ul>
+        {/*Get user's playlists via map & key' */}
+        {playlists.map(({ name, id }) => {
+          return (
+            <li key={id} onClick={() => changeCurrentPlaylist(id)}>
+              {name}
+            </li>
+          );
+        })}
+      </ul>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100%;
+  overflow: hidden;
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 55vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
+    li {
+      transition: 0.3s ease-in-out;
+      cursor: pointer;
+      &:hover {
+        color: white;
+      }
+    }
+  }
+`;
